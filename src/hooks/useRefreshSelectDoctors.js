58a94d90df0b7c54a9a9dc6e57f1450fc666414b
William <willrodriguess77@gmail.com>
2022-06-15 15:25:47 -0300

    Deixando apenas os arquivos de build

--- src/hooks/useRefreshSelectDoctors.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react'
-import api from '../services/api'
-
-const useRefreshSelectDoctors = id_especialidade => {
-        const response = await api.get('/medico/getDoctors', {id_especialidade})
-}
-
-export default useRefreshSelectDoctors
--- src/styles/components/NavBar/NavBar.js
+++
@@ -1,6 +0,0 @@
-import { styled } from '@mui/system'
-import { Box } from '@mui/material'
-
-export const Image = styled(Box)({
-    alignSelf: 'center'
-})
